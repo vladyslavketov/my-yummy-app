597c0a12158a6f50fe99da42070017b7e8fd8ead
vladyslavketov <106628608+vladyslavketov@users.noreply.github.com>
2023-05-30 12:25:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <BrowserRouter basename="yummy-app"> */}
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="yummy-app">
+//     <BrowserRouter basename="/">
       <Provider store={store}>
         <PersistGate loading={"null"} persistor={persistor}>
           <App />
